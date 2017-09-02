3cc9f6a97f8ece678331eddc4efc6d856962d30a
sparshturkane <rajaturkane99@gmail.com>
2017-09-02 09:11:06 +0530

    custom directive named bold directive created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { MovieslistService } from './services/movieslist/movieslist.service';
+import { BoldDirective } from './directives/bold/bold.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    BoldDirective
   ],
   imports: [
     BrowserModule,
